1bbc422f46e23a7ac42a168ed24ee490f157e75a
mildobread <mildobread@gmail.com>
2025-03-03 13:52:00 +0900

    add visitor tracking
    Signed-off-by: mildobread <mildobread@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 import BitcoinPrice from "./components/BitcoinPrice";
 
 function App() {
   return (
     <div className="App">
+      <Analytics/>
       <BitcoinPrice/>
     </div>
   );
